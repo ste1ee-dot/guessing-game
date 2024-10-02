8aa46aab9214bd8ac5000ba1797ca66b26e8bcdf
ste1ee-dot <master2005pop@gmail.com>
2024-10-02 08:38:23 -0700

    Added randomly generated answers

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
-use rand::Rng;
+use rand::seq::SliceRandom;
+use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
 use std::thread;
 use std::time::Duration;
@@ -9,6 +10,32 @@ fn main() {
     println!("Give me your best guess 1 - 1 000 000 000 :)");
 
     let secret_number = rand::thread_rng().gen_range(1..=1000000000);
+
+    let guess_high: [&str; 10] = [
+        "Too high! Try a lower number.",
+        "Nope, that's too big! Aim lower.",
+        "That's a bit too much! Lower your guess.",
+        "You're overshooting it! Try something smaller.",
+        "That's not quite right, it's too high.",
+        "Your guess is too high, dial it down a bit!",
+        "Whoa, too big! Go lower!",
+        "That's way too much! Aim for a smaller number.",
+        "You're getting warmer, but that's still too high!",
+        "Almost there, but you're still too high!"
+    ];
+
+    let guess_low: [&str; 10] = [
+        "Too low! Try a higher number.",
+        "Nope, that's too small! Aim higher.",
+        "That's a bit too little! Raise your guess.",
+        "You're undershooting it! Try something bigger.",
+        "That's not quite right, it's too low.",
+        "Your guess is too low, crank it up a bit!",
+        "Whoa, too small! Go higher!",
+        "That's way too little! Aim for a bigger number.",
+        "You're getting warmer, but that's still too low!",
+        "Almost there, but you're still too low!"
+    ];
 
     loop {
         println!("Please input your guess: ");
@@ -30,8 +57,8 @@ fn main() {
         println!("You gessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", guess_low.choose(&mut thread_rng()).unwrap()),
+            Ordering::Greater => println!("{}", guess_high.choose(&mut thread_rng()).unwrap()),
             Ordering::Equal => {
                 println!("You win!");
                 break;
